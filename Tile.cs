46fa05fee1292ebf751860bd2e2ce260a1a62ac3
Karan Shah <karanshah0206@outlook.com>
2021-10-22 14:42:11 +0530

    Added Abstract Tile Class

---
+++ Tile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace monopoly
+{
+    abstract class Tile
+    {
+        private int _loc;
+        private string _name;
+        public Tile(int loc, string name)
+        { _loc = loc; _name = name; }
+        public abstract void action(List<Player> players, int currentPlayerIndex, int diceValue);
+        public int Location
+        { get { return _loc; } }
+        public string Name
+        { get { return _name; } }
+    }
+}
