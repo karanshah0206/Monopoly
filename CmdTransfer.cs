3c0a7e5259278f8b6d5d00da0f44be3900d10f96
Karan Shah <karanshah0206@outlook.com>
2021-10-22 14:43:03 +0530

    Transfer Command Added

---
+++ CmdTransfer.cs
@@ -0,0 +1,15 @@
+﻿namespace monopoly
+{
+    static class CmdTransfer
+    {
+        public static void AddToAccount(Player p, int amount)
+        { p.Balance += amount; }
+        public static void MakePayment(Player payer, int amount)
+        { payer.Balance -= amount; }
+        public static void MakePayment(Player payer, int amount, Player payee)
+        {
+            MakePayment(payer, amount);
+            AddToAccount(payee, amount);
+        }
+    }
+}
