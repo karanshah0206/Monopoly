36c3236f5fb0bcf11c9b018743935f55ffa47d6a
Karan Shah <karanshah0206@outlook.com>
2021-10-22 14:42:45 +0530

    Added Balance Field To Player

--- Player.cs
+++ Player.cs
@@ -2,15 +2,20 @@
 {
     class Player
     {
-        private int _loc;
         private string _name;
-        public Player(string name)
-        { _name = name; _loc = 0; }
+        private int _loc, _balance;
+        public Player(string name, int balance)
+        { _name = name; _balance = balance; _loc = 0; }
         public void moveToLoc(int loc, bool passGo = false)
         { _loc = loc; }
         public void moveByCount(int count)
         { _loc += count; _loc %= 40; }
-        public int CurrentLocation
+        public int Location
         { get { return _loc; } }
+        public int Balance
+        {
+            get { return _balance; }
+            set { _balance = value; }
+        }
     }
 }
