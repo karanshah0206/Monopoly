230d6dd80e97627b9927d4f8fdd8a6bb0c7661ee
Karan Shah <karanshah0206@outlook.com>
2021-10-24 02:11:10 +0530

    Created Opportunity Card As Specialization For ICard With OpportunityCommand

---
+++ OpportunityCard.cs
@@ -0,0 +1,24 @@
+﻿namespace monopoly
+{
+    class OpportunityCard : ICard
+    {
+        private OpportunityCommand _command;
+        private string _title, _desc, _cmdString;
+
+        public OpportunityCard(string title, string desc, string command)
+        {
+            _title = title; _desc = desc;
+            _cmdString = command;
+            _command = new OpportunityCommand();
+        }
+
+        public void Execute(Player p)
+        { _command.Execute(p, _cmdString.Split(" ")); }
+
+        public string Title
+        { get { return _title; } }
+
+        public string Description
+        { get { return _desc; } }
+    }
+}
---
+++ OpportunityCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace monopoly
+{
+    class OpportunityCommand
+    {
+        public OpportunityCommand()
+        { }
+
+        public void Execute(Player p, string[] cmdString)
+        { /* Pass cmdString To Concrete Commands */ }
+    }
+}
