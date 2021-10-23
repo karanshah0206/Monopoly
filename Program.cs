b8c38fdd1c97b694c78f33c5da61f3cc92f9b769
Karan Shah <karanshah0206@outlook.com>
2021-10-24 02:49:47 +0530

    Revert "Revert "Player Can Store/Add/Remove Cards""
    This reverts commit e6bbf2f54c608f2ac651963b61a872b4830d6d90.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,21 @@
-using System;
-using SplashKitSDK;
+/*
+ * ACTIONS:
+ * Buy Transactionable Tile (Add To CMDTransfer)
+ * Sell Transactionable Tile
+ * Build House/Hotel (Thinking CMDBuildables)
+ * Sell House/Hotel
+ * 
+ * TODO:
+ * ^Cards (Make Relevant Changes to PurchasableTile, PropertyTile.cs, ServiceTile.cs, StationTile.cs)
+ * CmdCard (Make Relevant Changes In GoToJail.cs, OpportunityTile.cs, StationCard.cs, ServiceCard.cs)
+ * ^Deck (Make Relevant Changes In OpportunityTile.cs)
+ * Create Concrete Commands For Opportunity Command 
+ * Cards Factory (Make Relevant Changes in Tile Factory)
+ * Jail Manager (Make Relevant Changes In GoToJail.cs)
+ * Add Images To All Required Classes
+ * SidebarController (Make Relevant Changes In PurchasableTile)
+ * IDrawable + GUI
+ */
 
 namespace monopoly
 {
