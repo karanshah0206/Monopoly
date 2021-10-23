e6bbf2f54c608f2ac651963b61a872b4830d6d90
Karan Shah <karanshah0206@outlook.com>
2021-10-24 02:48:09 +0530

    Revert "Player Can Store/Add/Remove Cards"
    This reverts commit 90e33b84544ed9ae316d17a94e691050e718b697.

--- Player.cs
+++ Player.cs
@@ -1,38 +1,22 @@
-﻿using System.Collections.Generic;
-
-namespace monopoly
+﻿namespace monopoly
 {
     class Player
     {
         private string _name;
         private int _loc, _balance;
-        private List<ICard> _cards;
-
         public Player(string name, int balance)
         { _name = name; _balance = balance; _loc = 0; }
-
-        public void AddCard(ICard card)
-        { _cards.Add(card); }
-
-        public void RemoveCard(ICard card)
-        { _cards.Remove(card); }
-
         public string Name
         { get { return _name; } }
-
         public int Location
         {
             get { return _loc; }
             set { _loc = value; }
         }
-
         public int Balance
         {
             get { return _balance; }
             set { _balance = value; }
         }
-
-        public List<ICard> Cards
-        { get { return _cards; } }
     }
 }
