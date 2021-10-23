f27ee5b9374c089dbe9e64b3d2768b9388ed3cf2
Karan Shah <karanshah0206@outlook.com>
2021-10-24 03:22:07 +0530

    CmdMove Checks Added For To Ensure Go Salary

--- CmdMove.cs
+++ CmdMove.cs
@@ -3,9 +3,17 @@
     static class CmdMove
     {
         public static void MoveToLoc(Player p, int loc, bool passGo = true)
-        { p.Location = loc; ExecuteTileAction(p); }
+        {
+            if (passGo && p.Location > loc && loc != 0) Board.GetTile(0).Execute(p);
+            p.Location = loc; ExecuteTileAction(p);
+        }
+
         public static void MoveByCount(Player p, int count)
-        { p.Location += count; p.Location %= 40; ExecuteTileAction(p); }
+        {
+            if (p.Location >= (p.Location + count) % 40 && (p.Location + count) % 40 != 0) Board.GetTile(0).Execute(p);
+            p.Location += count; p.Location %= 40; ExecuteTileAction(p);
+        }
+
         private static void ExecuteTileAction(Player p)
         { Board.GetTile(p.Location).Execute(p); }
     }
