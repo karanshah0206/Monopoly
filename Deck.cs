d67dcf1d23043cb9fbd331b313f593a2a20ab9dc
Karan Shah <karanshah0206@outlook.com>
2021-10-24 02:35:43 +0530

    Created Deck Class

---
+++ Deck.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace monopoly
+{
+    class Deck
+    {
+        private Stack<ICard> _deck = new();
+
+        public Deck(List<ICard> cards)
+        { foreach (ICard card in cards) _deck.Push(card); }
+
+        public ICard GetTopCard()
+        { return _deck.Pop(); }
+
+        public void SendCardToBottom(ICard card)
+        {
+            Stack<ICard> temp = new();
+            while (_deck.Count > 0) temp.Push(_deck.Pop());
+            _deck.Push(card);
+            while (temp.Count > 0) _deck.Push(temp.Pop());
+        }
+    }
+}
