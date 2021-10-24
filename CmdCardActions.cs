13c7fd35b6c012549bdbee4b63d1c8a6599aad0c
Karan Shah <karanshah0206@outlook.com>
2021-10-25 00:42:26 +0530

    Created CommandInterpreter For Opportunity Cards + Initial Version of Card Actions Commands Created

---
+++ CmdCardActions.cs
@@ -0,0 +1,26 @@
+﻿namespace monopoly
+{
+    public class CmdCardActions
+    {
+        public static void AddCardToPlayer(Player p, ICard c)
+        { p.AddCard(c); }
+
+        public static int GetPlayerHouseCount(Player p)
+        { return 0; /* Add Logic */ }
+
+        public static int GetPlayerHotelCount(Player p)
+        { return 0; /* Add Logic */ }
+
+        public static void Chance(Player p)
+        { Board.ChanceDeck.GetTopCard().Execute(p); }
+
+        public static void CommunityChest(Player p)
+        { Board.CommunityChestDeck.GetTopCard().Execute(p); }
+
+        public static void ReturnCardToDeck(OpportunityCard c)
+        {
+            if (c.Title == "CHANCE") Board.ChanceDeck.SendCardToBottom(c);
+            else if (c.Title == "COMMUNITY CHEST") Board.CommunityChestDeck.SendCardToBottom(c);
+        }
+    }
+}
---
+++ CommandInterpreter.cs
@@ -0,0 +1,45 @@
+﻿namespace monopoly
+{
+    public static class CommandInterpreter
+    {
+        public static void Execute(Player p, OpportunityCard c)
+        {
+            string[] cmdString = c.Command.Split(" ");
+
+            switch(cmdString[0])
+            {
+                case "move": Move(p, cmdString); CmdCardActions.ReturnCardToDeck(c); break;
+                case "pay": case "earn": Transaction(p, cmdString); CmdCardActions.ReturnCardToDeck(c); break;
+                case "keep": Keep(p, c); break;
+            }
+        }
+
+        private static void Move(Player p, string[] cmdString)
+        {
+            if (cmdString[1] == "to")
+            {
+                if (cmdString.Length == 4 && cmdString[3] == "false") CmdMove.MoveToLoc(p, int.Parse(cmdString[2]), false);
+                else CmdMove.MoveToLoc(p, int.Parse(cmdString[2]));
+            }
+            else if (cmdString[1] == "by") CmdMove.MoveByCount(p, int.Parse(cmdString[2]));
+        }
+
+        private static void Transaction(Player p, string[] cmdString)
+        {
+            if (cmdString[0] == "earn") CmdTransfer.AddToAccount(p, int.Parse(cmdString[1]));
+            else if (cmdString[0] == "pay")
+            {
+                if (cmdString.Length == 3)
+                {
+                    int damage = int.Parse(cmdString[1]) * CmdCardActions.GetPlayerHouseCount(p);
+                    _ = int.Parse(cmdString[2]) * CmdCardActions.GetPlayerHotelCount(p);
+                    CmdTransfer.MakePayment(p, damage);
+                }
+                else CmdTransfer.MakePayment(p, int.Parse(cmdString[1]));
+            }
+        }
+
+        private static void Keep(Player p, OpportunityCard c)
+        { CmdCardActions.AddCardToPlayer(p, c); }
+    }
+}
