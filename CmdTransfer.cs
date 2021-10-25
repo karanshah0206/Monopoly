23c47911b8c24fe027d5ebc830845080989390b2
Karan Shah <karanshah0206@outlook.com>
2021-10-25 18:06:10 +0530

    Buy/Sell Property Feature Added

--- CmdTransfer.cs
+++ CmdTransfer.cs
@@ -2,10 +2,27 @@
 {
     public static class CmdTransfer
     {
+        public static void BuyProperty(Player p, PurchasableTile t)
+        {
+            MakePayment(p, t.Price);
+            t.Owner = p;
+            p.Cards.Add(t.Card);
+        }
+
+        public static void SellProperty(PurchasableTile t)
+        {
+            Player p = t.Owner;
+            if (t.GetType() == typeof(PropertyTile)) { /* Sell Buildables On All Of Colorgroup First */ }
+            t.Owner = null; p.Cards.Remove(t.Card);
+            AddToAccount(p, t.ResaleValue);
+        }
+
         public static void AddToAccount(Player p, int amount)
         { p.Balance += amount; }
+
         public static void MakePayment(Player payer, int amount)
         { payer.Balance -= amount; }
+
         public static void MakePayment(Player payer, int amount, Player payee)
         {
             MakePayment(payer, amount);
