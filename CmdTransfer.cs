c36782ac7292382d7d9b645d88fb4d37f4a8a7a6
Karan Shah <karanshah0206@outlook.com>
2021-10-25 20:32:04 +0530

    Check For Bankruptcy

--- CmdTransfer.cs
+++ CmdTransfer.cs
@@ -23,12 +23,41 @@
         { p.Balance += amount; }
 
         public static void MakePayment(Player payer, int amount)
-        { payer.Balance -= amount; }
+        {
+            if (payer.Balance >= amount) payer.Balance -= amount;
+            else throw new System.Exception("Not enough money.");
+        }
 
         public static void MakePayment(Player payer, int amount, Player payee)
         {
             MakePayment(payer, amount);
             AddToAccount(payee, amount);
+        }
+
+        public static bool IsBankrupt(Player p, int paymentDue)
+        {
+            int networth = p.Balance;
+
+            for (int i = 0; i < 40; i++)
+            {
+                if (Board.GetTile(i).GetType() == typeof(PropertyTile))
+                {
+                    PropertyTile t = Board.GetTile(i) as PropertyTile;
+                    PropertyCard c = t.Card as PropertyCard;
+
+                    if (t.Owner == p) networth += t.ResaleValue;
+                    networth += (c.BuildableCost / 2) * t.HouseCount;
+                    if (t.HasHotel) networth += c.BuildableCost / 2;
+                }
+                else if (Board.GetTile(i).GetType() == typeof(StationTile) || Board.GetTile(i).GetType() == typeof(ServiceTile))
+                {
+                    PurchasableTile t = Board.GetTile(i) as PurchasableTile;
+                    if (t.Owner == p) networth += t.ResaleValue;
+                }
+            }
+
+            if (networth < paymentDue) return true;
+            return false;
         }
     }
 }
