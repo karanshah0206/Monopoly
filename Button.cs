eadf6623b5c40b6a932ac167dc9b5a120cbe9c24
Karan Shah <karanshah0206@outlook.com>
2021-11-01 10:46:27 +0530

    Added Button Class

---
+++ Button.cs
@@ -0,0 +1,23 @@
+﻿using SplashKitSDK;
+
+namespace monopoly
+{
+    class Button : IDrawable
+    {
+        private Color _clr;
+        private string _text;
+        private int _x, _y;
+
+        public Button(Color clr, string text, int x, int y)
+        {
+            _clr = clr; _text = text;
+            _x = x; _y = y;
+        }
+
+        public void Draw()
+        {
+            SplashKit.FillRectangle(_clr, _x, _y, 150, 25);
+            SplashKit.DrawText(_text, Color.Black, "Roboto", 15, _x + 10, _y + 4);
+        }
+    }
+}
