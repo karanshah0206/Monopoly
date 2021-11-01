6c9f4ea82c233c5978922d67fa67e8183304d081
Karan Shah <karanshah0206@outlook.com>
2021-11-01 13:28:20 +0530

    Go Tile Only Calls Next Player if Direct Landing

--- CmdMove.cs
+++ CmdMove.cs
@@ -4,13 +4,13 @@
     {
         public static void MoveToLoc(Player p, int loc, bool passGo = true)
         {
-            if (passGo && p.Location > loc && loc != 0) Board.GetTile(0).Execute(p);
+            if (passGo && p.Location > loc && loc != 0) ((GoTile)Board.GetTile(0)).Execute(p, false);
             p.Location = loc; ExecuteTileAction(p);
         }
 
         public static void MoveByCount(Player p, int count)
         {
-            if (p.Location >= (p.Location + count) % 40 && (p.Location + count) % 40 != 0) Board.GetTile(0).Execute(p);
+            if (p.Location >= (p.Location + count) % 40 && (p.Location + count) % 40 != 0) ((GoTile)Board.GetTile(0)).Execute(p, false);
             p.Location += count; p.Location %= 40; ExecuteTileAction(p);
         }
 
